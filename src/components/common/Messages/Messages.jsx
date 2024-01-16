3d5334e540326dfb4ccba7450e40bd1892afa809
csanbardev <csanbar97@gmail.com>
2024-01-16 17:01:47 +0100

    create message to show generated url

--- src/components/common/Messages/Messages.jsx
+++ src/components/common/Messages/Messages.jsx
@@ -1,12 +1,13 @@
 import './Messages.css'
-import { MdError, MdAnnouncement, MdDone } from "react-icons/md";
+import { MdError, MdAnnouncement, MdDone, MdContentCopy } from "react-icons/md";
+import copy from 'clipboard-copy'
 
 export function CommonMessage({ text, type }) {
 
   const icons = {
     "error": <MdError color='white' size={15} className='message-icon' />,
     "info": <MdAnnouncement color='white' size={15} className='message-icon' />,
-    "success":  <MdDone color='white' size={15} className='message-icon' />
+    "success": <MdDone color='white' size={15} className='message-icon' />
   }
 
   const types = {
@@ -16,9 +17,26 @@ export function CommonMessage({ text, type }) {
   }
 
   return (
-    <div style={{backgroundColor: types[type]}} className='message-container'>
+    <div style={{ backgroundColor: types[type] }} className='message-container'>
       {icons[type]}
       <p>{text}</p>
     </div>
+  )
+}
+
+export function CopyMessage({ text }) {
+
+  const handleCopyClick = () => {
+    copy(text)
+  }
+
+  return (
+    <>
+      <div className='copy-message-container'>
+        <p>Enlace generado</p>
+        <input className='url-input' type="text" placeholder={text} disabled />
+        <button type='button' className='button--submit' onClick={handleCopyClick} ><MdContentCopy /></button>
+      </div>
+    </>
   )
 }
