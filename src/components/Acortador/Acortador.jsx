8cd1497571c18a6294777ebe638dafc954172ad6
csanbardev <csanbar97@gmail.com>
2024-01-17 11:36:18 +0100

    change banner img & change h1 from banner to acortador comp.

--- src/components/Acortador/Acortador.jsx
+++ src/components/Acortador/Acortador.jsx
@@ -4,6 +4,7 @@ import './Acortador.css'
 export function Acortador() {
   return(
     <section id='acortador'>
+      <h1>Acorta tu URL</h1>
       <AcortarForm />
     </section>
   )
--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -5,7 +5,6 @@ export function Banner(){
   return(
     <section id='banner'>
       <img className='banner-img' src={bannerImage} alt="" />
-      <h1>Acorta tu URL</h1>
     </section>
   )
 }
