7c5dccbc1e5048bd3f74bd3258161d396a9e20d9
csanbardev <csanbar97@gmail.com>
2024-01-26 13:54:59 +0100

    add new elements & styles

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -5,6 +5,8 @@ export function Banner(){
   return(
     <section id='banner'>
       <img className='banner-img' src={bannerImage} alt="" />
+      <h1 className='banner-title'>Petit</h1>
+      <p>Acorta tus imágenes de manera rápida y gratuita.</p>
     </section>
   )
 }
