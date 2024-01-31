3b3ef5679b1c83db5140695bf4573b8f6b46f646
csanbardev <csanbar97@gmail.com>
2024-01-31 13:57:53 +0100

    solve misprint

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -6,7 +6,7 @@ export function Banner(){
     <section id='banner'>
       <img className='banner-img' src={bannerImage} alt="" />
       <h1 className='banner-title'>Petit</h1>
-      <p>Acorta tus imágenes de manera rápida y gratuita.</p>
+      <p>Acorta tus enlaces de manera rápida y gratuita.</p>
     </section>
   )
 }
