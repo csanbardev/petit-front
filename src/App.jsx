9552af6f9778d4ba6bdc2358d895ad1d535deee0
csanbardev <csanbar97@gmail.com>
2024-02-02 11:48:33 +0100

    implement footer at home

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import { About } from './components/About/About'
 import { Acortador } from './components/Acortador/Acortador'
 import { Banner } from './components/Banner/Banner'
+import { Footer } from './components/Footer/Footer'
 import { Nav } from './components/Nav/Nav'
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
     <Banner />
     <Acortador />
     <About />
+    <Footer />
     </>
   )
 }
